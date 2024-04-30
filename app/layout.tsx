98fb49e8c6ee85670c1127ef66e7bba3a11e4b47
Deepak <deepakv2001@gmail.com>
2024-04-30 10:01:34 +0530

    Adding speed insight

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,5 @@
 import './globals.css'
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 export default function RootLayout({
   children,
@@ -7,6 +8,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      < SpeedInsights />
       <body>{children}</body>
     </html>
   )
